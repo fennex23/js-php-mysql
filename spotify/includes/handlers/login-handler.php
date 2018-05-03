4d06d4322d1b89510a7b8c28c6fe7a4a021e762c
fennex23 <nfp.fennex@gmail.com>
2018-05-03 19:27:25 +0200

    commit error removal

--- spotify/includes/handlers/login-handler.php
+++ spotify/includes/handlers/login-handler.php
@@ -1,4 +1,5 @@
 <?php
     if(isset($_POST['loginButton'])){
         // login button was pressed
+    }
 ?>
--- spotify/includes/handlers/register-handler.php
+++ spotify/includes/handlers/register-handler.php
@@ -1,18 +1,17 @@
-<?php^
-
+<?php
     function sanitizeFormUsername($inputText){
-        $inputText = strip_tags($inputText]);
+        $inputText = strip_tags($inputText);
         $inputText = str_replace(" ", "", $inputText);
         return $inputText;
     }
     function sanitizeFormString($inputText){
-        $inputText = strip_tags($inputText]);
+        $inputText = strip_tags($inputText);
         $inputText = str_replace(" ", "", $inputText);
         $inputText = ucfirst(strtolower($inputText));
         return $inputText;
     }
     function sanitizeFormPassword($inputText){
-        $inputText = strip_tags($inputText]);
+        $inputText = strip_tags($inputText);
         return $inputText;
     }
     if(isset($_POST['registerButton'])){
