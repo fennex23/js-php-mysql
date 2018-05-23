21c042ab4650c018f34d51844dbf2403e61aec93
fennex23 <nfp.fennex@gmail.com>
2018-05-23 21:55:09 +0200

    title changed

--- spotify/register.php
+++ spotify/register.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to Fenschify</title>
+    <title>Welcome to Fenschify 2.0</title>
 </head>
 <body>
    <div id="inputContainer">
